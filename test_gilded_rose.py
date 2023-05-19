840e839f543ba21f77651f9e7179e0cdb9acc696
TT <tony.trinh58@gmail.com>
2023-05-18 18:52:11 -0700

    Added and seemingly finished unit tests. Added Documentation.txt for thought process.

--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -31,6 +31,84 @@ class GildedRoseTest(unittest.TestCase):
         gilded_rose.update_quality()
         self.assertEqual(3, items[0].quality)
 
+    def test_array_count(self):
+        items = [Item("foo", 0, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("foo", items[0].name)
+        self.assertEqual(3, items[0].quality)
+        self.assertEqual(1, len(items))
+
+    # Aged Brie should go up in quality while the sell in should go down.
+    def test_aged_brie(self):
+        items = [Item("Aged Brie", 1, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(1, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(3, items[0].quality)
+
+    def test_quality_fifty(self):
+        items = [Item("Aged Brie", 1, 50)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(0, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)
+
+    # Sulfuras shouldn't go down in quality or sell in.
+    def test_sulfuras(self):
+        items = [Item("Sulfuras, Hand of Ragnaros", 10, 80)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(10, items[0].sell_in)
+        self.assertEqual(80, items[0].quality)
+
+    # Checks quality of the backstage passes when over 10. Also makes sure that
+    # it doesn't go over the 50 quality mark.
+    def test_backstage_ten_more(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 11, 48)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(10, items[0].sell_in)
+        self.assertEqual(49, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)
+        gilded_rose.update_quality()
+        self.assertEqual(8, items[0].sell_in)
+        self.assertEqual(50, items[0].quality)
+
+    def test_backstage_ten_less(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 10, 2)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(4, items[0].quality)
+
+    # Increases by 3 when less than 5 days.
+    def test_backstage_five_less(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 5, 0)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(4, items[0].sell_in)
+        self.assertEqual(3, items[0].quality)
+
+    # 0 after concert is over.
+    def test_backstage_zero(self):
+        items = [Item("Backstage passes to a TAFKAL80ETC concert", 0, 10)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(-1, items[0].sell_in)
+        self.assertEqual(0, items[0].quality)
         
+    def test_conjured(self):
+        items = [Item("Conjured Mana Cake", 10, 5)]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual(9, items[0].sell_in)
+        self.assertEqual(3, items[0].quality)
+
 if __name__ == '__main__':
     unittest.main()
